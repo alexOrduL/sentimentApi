a159ed0a04c134cfce61e326fbf21274618304b7
alexOrduL <aordunol95@gmail.com>
2025-06-27 14:51:51 -0600

    feat: initialize project structure and config

---
+++ SentimentApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using SentimentApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Forzar escucha en todas las IPs del contenedor
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+// Agregar servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configurar EF Core con SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+Console.WriteLine("Connection string:");
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+// Usar Swagger solo en desarrollo o siempre si prefieres
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
